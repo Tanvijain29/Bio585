7ddc73ae8184a7f24db5ce79cbe6e2210ce1aa24
Tanvijain29 <tanvi.29701@gmail.com>
2025-02-04 12:51:48 -0500

    removed edit_git_config
    I don't need these lines so removed them.

--- CompBio/lab2.R
+++ CompBio/lab2.R
@@ -64,7 +64,5 @@ lines(density(norm4), col = "green")
 
 
 use_git_config(user.name = "Tanvijain29", user.email = "tanvi.29701@gmail.com")
-edit_git_config()
 use_git()
-
 options("install.lock"=FALSE)
